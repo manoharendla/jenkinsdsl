cffcb9c273ebb2b24109530260efa1648f1997b1
mano <manoharendla277@gmail.com>
2022-05-30 21:06:24 +0530

    Create test2.groovy

---
+++ test2.groovy
@@ -0,0 +1,27 @@
+job('mano-1') {
+    description 'Build and test the app.'
+    configure { project -> 
+       project / 'buildWrappers' / 'EnvInjectPasswordWrapper' {
+         injectGlobalPasswords ('true')
+         maskPasswordParameters ('true')           
+            
+        }
+    }
+    scm {
+        git {
+            remote {
+                github('account/repo', 'https')
+                credentials('mano-github')
+                branch('*/main')
+            }
+        }
+    }
+    steps {
+        sh '''
+        echo "hello"
+        python --version
+        pip install -r requirements.txt
+        '''
+    }
+    
+}
